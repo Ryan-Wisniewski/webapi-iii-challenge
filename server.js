5f596a0cf3051d31e289484847a86306c14af6f7
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-04 18:36:47 -0400

    I dont even really know where to start :O this will be fun tonight.

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('origin')}`
 };
+
+function validateUserId(res, req, next)  {
+  
+}
+
+function validateUser(res, req, next)  {
+  
+}
+
+function validatePost(res, req, next)  {
+  
+}
 
 module.exports = server;
